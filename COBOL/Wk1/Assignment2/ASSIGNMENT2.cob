df58bdc2678d61019a4ea5a43ed5e29d3c8d959e
Giddy-K <gideonkipamet@gmail.com>
2026-07-11 05:04:59 +0300

    Create ASSIGNMENT2.cob

---
+++ COBOL/Wk1/Assignment2/ASSIGNMENT2.cob
@@ -0,0 +1,12 @@
+      *> AUTHOR: Gideon
+      *> DATE: July 01, 2026
+      *> PURPOSE: My first COBOL program - display greetings
+
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. ASSIGNMENT2.
+
+       PROCEDURE DIVISION.
+           DISPLAY "Welcome to FAC Academy COBOL!".
+           DISPLAY "I am learning COBOL.".
+      *> Program ends here
+           STOP RUN.
